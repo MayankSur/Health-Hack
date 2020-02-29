2e957665c9b378a4543cac78c8f2b689c9f0802c
Ivan <ivan.busha18@imperial.ac.uk>
2020-02-29 22:09:34 +0000

    merging the front end

--- app/user/app.py
+++ app/user/app.py
@@ -2,59 +2,55 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 
 app = Flask(__name__)
 app.secret_key ="ICHACK_2020"
-from werkzeug.debug import DebuggedApplication
-app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
-app.debug = True
+
+user_names=["Ivan"]
+user_password=["1hjnf4dpdjl"]
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("login1.html")
 
 @app.route("/login",methods=["POST","GET"])
 def login():
 	if request.method=="POST":
-		user = request.form["nm"]
-		session["user"] = user
-		flash("Login Succesful!")
-		return redirect(url_for("user"))
+		user_test = request.form["nm"]
+		password_test = request.form["ps"]
+		if user_test==user_names[0] and password_test==user_password[0]:
+			session["user"] = user_test
+			return render_template("general.html",user=user_test)
+		else:
+			flash("Wrong username or password! Try again!")
+			return render_template("login1.html")
 	else:
 		if "user" in session:
 			flash("Already logged in")
 			return redirect(url_for("user"))
-		return render_template("login.html")
+		return render_template("login1.html")
 
-@app.route("/user")
-def user():
-	if "user" in session:
-		user = session["user"]
-		return render_template("user.html",user=user)
-	else:
-		flash("You Are not logged in")
-		return redirect(url_for("login"))
+@app.route("/general")
+def general():
+	user=session["user"]
+	return render_template("general.html",user=user)
+
+@app.route("/info")
+def info():
+	user=session["user"]
+	return render_template("info.html",user=user)
 
 @app.route("/tests")
 def tests():
-	if "user" in session:
-		return render_template("tests.html")
-	else:
-		flash("You Are not logged in")
-		return redirect(url_for("login"))
+	user=session["user"]
+	return render_template("tests.html",user=user)
 
-@app.route("/gp")
-def gp():
-	if "user" in session:
-		return render_template("gp.html")
-	else:
-		flash("You Are not logged in")
-		return redirect(url_for("login"))		
+@app.route("/settings")
+def settings():
+	user=session["user"]
+	return render_template("settings.html",user=user)
 
 @app.route("/logout")
 def logout():
-	if "user" in session:
-		user = session["user"]
-		flash(f"You have been logged out, {user}!", "info")
 	session.pop("user",None)
-	return redirect(url_for("login"))
+	return render_template("login1.html")
 
 if __name__=="__main__":
      app.run(debug=True)
